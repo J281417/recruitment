8a85aa5f14f0101138a9f6e161bbfb78f9d035ea
Noland Rimando <J281417@tafe.wa.edu.au>
2025-11-23 11:28:05 +0800

    Added TestAddContractor

---
+++ test/Test1.cs
@@ -0,0 +1,23 @@
+﻿
+using recruitment;
+
+namespace test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+
+        [TestMethod]
+        public void TestAddContractor()
+        {
+            RecruitmentSystem recruitmentSystem = new RecruitmentSystem();
+
+            var contractor = new Contractor("Peter", "Parker", DateTime.Now.AddMonths(-12), 45);
+            recruitmentSystem.AddContractor(contractor);
+
+            Assert.AreEqual(recruitmentSystem.GetContractors().Count, 1);
+            Assert.IsTrue(recruitmentSystem.GetContractors().Contains(contractor));
+        }
+
+    }
+}
